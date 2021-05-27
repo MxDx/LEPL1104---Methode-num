0429072d38077439565c4ee2af27b9a94ca601d5
MxDx <maxime.dxl2002@gmail.com>
2021-05-27 09:28:32 +0200

    Third commit

--- S8/epidemicTest.py
+++ S8/epidemicTest.py
@@ -193,7 +193,8 @@ m = 10; n = Xend * m
 
 X,U = epidemicRungeKutta(Xstart,Xend,Ustart,n,beta,gamma)
 
-print("Time : {} [s]".format(time.time() - start))
+print("Time : {:e} [s]".format(time.time() - start))
+
 
 #
 # -3.1- Données du Japon à partir du 22 janvier 2020
